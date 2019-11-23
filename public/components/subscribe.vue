bac25f0e1e0acdb68233b1681be37817c8d67012
Alban Mouton <alban.mouton@gmail.com>
2019-11-23 14:14:03 +0100

    feat: subscribe iframe supports multiple topics

---
+++ public/components/subscribe.vue
@@ -0,0 +1,73 @@
+<template>
+  <v-col class="py-0 px-1">
+    <v-subheader class="pt-2 px-0" style="height: auto;">
+      Me notifier pour {{ topic.title }}.
+    </v-subheader>
+    <v-row v-if="subscription">
+      <v-col class="py-0">
+        <v-switch
+          v-model="subscription.outputs" dense hide-details class="mt-0 mb-1" label="navigateur"
+          value="web"
+          @change="patch"
+        />
+      </v-col>
+      <v-col class="pt-0 pb-2">
+        <v-switch
+          v-model="subscription.outputs" dense hide-details class="mt-0 mb-1" label="email"
+          value="email" @change="patch"
+        />
+      </v-col>
+    </v-row>
+    <v-progress-circular v-else indeterminate size="20" color="primary" />
+  </v-col>
+</template>
+
+<script>
+import { mapState, mapGetters } from 'vuex'
+
+export default {
+  props: {
+    topic: { type: Object, default: null }
+  },
+  data: () => ({
+    subscription: null
+  }),
+  computed: {
+    ...mapState('session', ['user']),
+    ...mapGetters('session', ['activeAccount'])
+  },
+  async mounted () {
+    this.refresh()
+  },
+  methods: {
+    async refresh () {
+      const params = {
+        recipient: this.user.id,
+        senderType: this.activeAccount.type,
+        senderId: this.activeAccount.id,
+        topic: this.topic.key
+      }
+      this.subscription = (await this.$axios.$get('api/v1/subscriptions', { params })).results[0]
+      if (!this.subscription) {
+        this.subscription = {
+          recipient: { id: this.user.id, name: this.user.name },
+          sender: this.activeAccount,
+          topic: this.topic,
+          outputs: []
+        }
+      }
+    },
+    async patch () {
+      if (this.subscription.outputs.length) {
+        await this.$axios.$post('api/v1/subscriptions', this.subscription)
+      } else if (this.subscription._id) {
+        await this.$axios.$delete('api/v1/subscriptions/' + this.subscription._id)
+      }
+      this.refresh()
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
--- public/pages/embed/subscribe.vue
+++ public/pages/embed/subscribe.vue
@@ -1,67 +1,22 @@
 <template>
   <v-container fluid data-iframe-height class="py-0">
-    <v-subheader class="px-0">
-      Me notifier pour {{ $route.query.title }}.
-    </v-subheader>
-    <v-row v-if="subscription">
-      <v-col class="py-0">
-        <v-switch
-          v-model="subscription.outputs" dense hide-details class="mt-0 mb-1" label="navigateur"
-          value="web"
-          @change="patch"
-        />
-      </v-col>
-      <v-col class="py-0">
-        <v-switch
-          v-model="subscription.outputs" dense hide-details class="mt-0 mb-1" label="email"
-          value="email" @change="patch"
-        />
-      </v-col>
+    <v-row v-for="topic in topics" :key="topic.key">
+      <subscribe :topic="topic" />
     </v-row>
-    <v-progress-circular v-else indeterminate size="20" color="primary" />
   </v-container>
 </template>
 
 <script>
-import { mapState, mapGetters } from 'vuex'
+import Subscribe from '~/components/subscribe.vue'
 
 export default {
   layout: 'embed',
-  data: () => ({
-    subscription: null
-  }),
+  components: { Subscribe },
   computed: {
-    ...mapState('session', ['user']),
-    ...mapGetters('session', ['activeAccount'])
-  },
-  async mounted () {
-    this.refresh()
-  },
-  methods: {
-    async refresh () {
-      const params = {
-        recipient: this.user.id,
-        senderType: this.activeAccount.type,
-        senderId: this.activeAccount.id,
-        topic: this.$route.query.key
-      }
-      this.subscription = (await this.$axios.$get('api/v1/subscriptions', { params })).results[0]
-      if (!this.subscription) {
-        this.subscription = {
-          recipient: { id: this.user.id, name: this.user.name },
-          sender: this.activeAccount,
-          topic: { key: this.$route.query.key, title: this.$route.query.title },
-          outputs: []
-        }
-      }
-    },
-    async patch () {
-      if (this.subscription.outputs.length) {
-        await this.$axios.$post('api/v1/subscriptions', this.subscription)
-      } else if (this.subscription._id) {
-        await this.$axios.$delete('api/v1/subscriptions/' + this.subscription._id)
-      }
-      this.refresh()
+    topics () {
+      const keys = this.$route.query.key.split(',')
+      const titles = this.$route.query.title.split(',')
+      return keys.map((key, i) => ({ key, title: titles[i] }))
     }
   }
 }
