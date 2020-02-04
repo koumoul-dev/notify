c91fb887aadd26a49cca73b9494ff37a239f86a3
Alban Mouton <alban.mouton@gmail.com>
2020-02-04 10:05:55 +0100

    fix: bug in mongo index management

--- server/utils/db.js
+++ server/utils/db.js
@@ -7,7 +7,8 @@ exports.ensureIndex = async (db, collection, key, options = {}) => {
   try {
     await db.collection(collection).createIndex(key, options)
   } catch (err) {
-    if ((err.code !== 85 && err.code !== 86) || !options.name) throw err
+    if (err.code === 85) return console.warning(err.message)
+    if (err.code !== 86 || !options.name) throw err
 
     // if the error is a conflict on keys or params of the index we automatically
     // delete then recreate the index
